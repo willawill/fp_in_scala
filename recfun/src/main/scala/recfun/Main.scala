5cf709bf17f5594bfb6ecf6863266cac5f82464a
Willa <mowmowjoker@gmail.com>
2013-04-02 22:31:46 -0400

    E2 for Assignment 1

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -15,6 +15,9 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
+    if (c < 0 || r < 0 || c > r) throw new NoSuchElementException
+    if (c > Int.MaxValue || r > Int.MaxValue) throw new NoSuchElementException("Int out of bounds")
+
     if (c == 0 || r == 0 || c == r) 1
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
@@ -22,7 +25,22 @@ object Main {
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    if (chars.isEmpty) throw new NoSuchElementException
+
+    def parens(left_counter: Int, right_counter: Int, remain: List[Char]) : Boolean = {
+      if (left_counter >= right_counter){
+        if (remain.isEmpty && left_counter == right_counter) true
+        else {
+          if (remain.head == '(') parens(left_counter + 1, right_counter, remain.tail)
+          else if (remain.head == ')') parens(left_counter, right_counter + 1, remain.tail)
+          else parens(left_counter, right_counter, remain.tail)
+        }
+      }
+      else false
+    }
+    parens(0, 0, chars)
+  }
 
   /**
    * Exercise 3
